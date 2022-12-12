4c6ea5dc762ae411f6dd3823a04c773589474db7
sh1eldma1den <@pOOsa856100>
2022-12-12 10:56:13 -0500

    changes on main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-import ExternalServices from './ExternalServices'
-import AnimalList from './animalList'
+import ExternalServices from './ExternalServices.js'
+import AnimalList from './animalList.js'
 
 // Create the product list
 const parentNode = document.querySelector('.animal-listing')
 const selector = '#animal-card-template'
 const externalServices = new ExternalServices()
-const animalList = new AnimalList(name, externalServices, parentNode)
+const animalList = new AnimalList(selector, externalServices, parentNode)
 
 document.getElementById('#submitName').addEventListener('submit', e => {
     animalList.init(selector)
