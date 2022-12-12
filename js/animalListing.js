768f46a9e1c7b0c168970973fd3f214ba5c3f055
sh1eldma1den <@pOOsa856100>
2022-12-12 10:40:44 -0500

    event listener changes

--- js/animalListing.js
+++ js/animalListing.js
@@ -6,7 +6,7 @@ import AnimalList from './animalList'
 
 // Create the product list
 const parentNode = document.querySelector('.animal-listing')
-const name = '#animal-card-template'
+const selector = '#animal-card-template'
 const externalServices = new ExternalServices()
 const animalList = new AnimalList(name, externalServices, parentNode)
 
--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-
+import ExternalServices from './ExternalServices'
 import AnimalList from './animalList'
 
+// Create the product list
+const parentNode = document.querySelector('.animal-listing')
+const selector = '#animal-card-template'
+const externalServices = new ExternalServices()
+const animalList = new AnimalList(name, externalServices, parentNode)
+
 document.getElementById('#submitName').addEventListener('submit', e => {
-    AnimalList.init(selector)
+    animalList.init(selector)
 })
