a46230ed979072b4f5d2a93da52fe7ae659ae20d
sh1eldma1den <@pOOsa856100>
2022-12-12 11:22:29 -0500

    another typo in external services

--- js/ExternalServices.js
+++ js/ExternalServices.js
@@ -1,5 +1,5 @@
 
-document.querySelector('submitName').addEventListener('submit', e => {
+document.querySelector('#submitName').addEventListener('submit', e => {
   e.preventDefault();
   const name = document.getElementById('searchName')
   const request = require('request');
