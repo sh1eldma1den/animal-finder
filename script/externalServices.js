1d41f3b7f7521d6c7c216a8bc2eac4d544e3f87a
sh1eldma1den <@pOOsa856100>
2022-12-13 12:22:12 -0500

    imported function

--- script/externalServices.js
+++ script/externalServices.js
@@ -1,16 +1,16 @@
+import { getAnimal } from "botanic-zoo-api"
 
 
-const card = document.getElementById('searchResultTop')
+// const card = document.getElementById('searchResultTop')
 const nameSearch = document.getElementById('submitName')
-const cardTemplate = document.getElementById('animal-card-template')
+// const cardTemplate = document.getElementById('animal-card-template')
 const animalInput = document.getElementById('searchName').value
-const myList = document.getElementById('my-list-total')
-const botanicZoo = ('botanic-zoo-api')
+// const myList = document.getElementById('my-list-total')
+// const botanicZoo = ('botanic-zoo-api')
 
 nameSearch.addEventListener('click', searchAnimal)
-function searchAnimal(animalInput){
-  botanicZoo
-    .getAnimal(animalInput)
+function searchAnimal(){
+  getAnimal(animalInput)
     .then((response) => console.log(response))
     .catch((err) => console.error(err))
 }
