445cfe73e738870d28a73339b9e3638a3689ddb6
sh1eldma1den <@pOOsa856100>
2022-12-13 19:19:18 -0500

    api change

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-import {request} from ('request')
+
 const nameSearch = document.getElementById('submitName')
 nameSearch.addEventListener('click', getAllAnimals)
 
@@ -8,7 +8,7 @@ function getAllAnimals() {
     const options = {
       method: 'GET',
       url: 'https://animals-by-api-ninjas.p.rapidapi.com/v1/animals',
-      qs: {name: animalInput},
+      qs: {name: 'cheetah'},
       headers: {
         'X-RapidAPI-Key': '3ef67f7eeamsh8b4f075f34e3a7dp18e177jsn3a434a265872',
         'X-RapidAPI-Host': 'animals-by-api-ninjas.p.rapidapi.com',
