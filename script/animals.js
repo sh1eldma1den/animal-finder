6927ebbb784ae94c79a53d3c86c603fc8168afcf
sh1eldma1den <@pOOsa856100>
2022-12-14 14:10:32 -0500

    css

--- script/animals.js
+++ script/animals.js
@@ -1,5 +1,46 @@
 
-import { loadHeaderFooter } from './utils.js'
+
+document.getElementById('submitImg').addEventListener('click', getImage);
 
 
-loadHeaderFooter('./partials/header.html', './partials/footer.html', true)
+export async function getImage() {
+    let animal = document.getElementById('searchImg').value;
+    const url = 'https://api.pexels.com/v1/search?query=' + animal;           
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3ef67f7eeamsh8b4f075f34e3a7dp18e177jsn3a434a265872',
+            'X-RapidAPI-Host': 'animals-by-api-ninjas.p.rapidapi.com',
+            'Content-Type': 'application/json'
+        },
+      };
+        const response = await fetch(url, options);
+        let data = await response.json();
+        console.log(data);
+        if(response){
+        renderAnimals(data);
+        // .then(data => {
+        //     renderAnimals(data);
+        // })
+    }
+}
+
+function renderAnimals(data) {
+        let card = document.getElementById('animal-listing');
+        let templateA = ''
+        data.forEach (a => {    
+        templateA += `<li class="animal-card">
+            <h3 class="card-header">Name: ${a.name}</h3>
+            <p>Locations: ${a.locations}</p>
+            <p>Prey: ${a.characteristics.prey}</p>
+            <p>Group Behavior: ${a.characteristics.group_behavior}</p>
+            <p>Biggest Threat: ${a.characteristics.biggest_threat}</p>
+            <p>Habitat: ${a.characteristics.habitat}</p>
+            <p>Diet: ${a.characteristics.diet}</p>
+            <p>Lifestyle: ${a.characteristics.lifestyle}</p>
+            <p><em>${a.characteristics.slogan}</em></p>
+            </li>`;
+        })
+            card.innerHTML = templateA;
+
+        }
