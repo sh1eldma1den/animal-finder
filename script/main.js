2b88b02a2cc7b01d87087c5340c88e5bf4f0b458
sh1eldma1den <@pOOsa856100>
2022-12-14 12:29:43 -0500

    fixed finally

--- script/main.js
+++ script/main.js
@@ -1,52 +1,46 @@
 
+
 document.getElementById('submitName').addEventListener('click', getAnimals);
 
 
-export function getAnimals() {
+export async function getAnimals() {
     let animal = document.getElementById('searchName').value;
-               
-        const url = 'https://animals-by-api-ninjas.p.rapidapi.com/v1/animals/' + animal;
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': '3ef67f7eeamsh8b4f075f34e3a7dp18e177jsn3a434a265872',
-                'X-RapidAPI-Host': 'animals-by-api-ninjas.p.rapidapi.com',
-                'Content-Type': 'application/json'
-            },
-        };
-        fetch(url, options)
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-                }
-        })
-        .then(json=> renderAnimals(json))
-        .catch ((response) => {
-            console.log(response.status);
-        });
-    }    
-
-    function renderAnimals(animals) {
-        const card = document.getElementById('animal-listing');
-        console.log(animals.results)
-        animals.results.forEach((animal) => {
-            let row = '';
-	        row += `<li class="animal-card">
-            <h3>${animals.animal.name}</h3>
-            <p>Locations: ${animals.animal.locations}</p>
-            <p>Prey: ${animals.animal.characteristics.prey}</p>
-            <p>Group Behavior: ${animals.animal.characteristics.group_behavior}</p>
-            <p>Biggest Threat: ${animals.animal.characteristics.biggest_threat}</p>
-            <p>Habitat: ${animals.animal.characteristics.habitat}</p>
-            <p>Diet: ${animals.animal.characteristics.diet}</p>
-            <p>Lifestyle: ${animals.animal.characteristics.lifestyle}</p>
-            <p>Fun fact: ${animals.animal.characteristics.slogan}</p>`
-            row += '</li>'
-            card.innerHTML = row;
-
-        });
+    const url = 'https://animals-by-api-ninjas.p.rapidapi.com/v1/animals?name=' + animal;           
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3ef67f7eeamsh8b4f075f34e3a7dp18e177jsn3a434a265872',
+            'X-RapidAPI-Host': 'animals-by-api-ninjas.p.rapidapi.com',
+            'Content-Type': 'application/json'
+        },
+      };
+        const response = await fetch(url, options);
+        let data = await response.json();
+        console.log(data);
+        if(response){
+        renderAnimals(data);
+        // .then(data => {
+        //     renderAnimals(data);
+        // })
     }
+}
 
-    
+function renderAnimals(data) {
+        let card = document.getElementById('animal-listing');
+        let templateA = ''
+        data.forEach (a => {    
+        templateA += `<li class="animal-card">
+            <h3 class="card-header">Name: ${a.name}</h3>
+            <p>Locations: ${a.locations}</p>
+            <p>Prey: ${a.characteristics.prey}</p>
+            <p>Group Behavior: ${a.characteristics.group_behavior}</p>
+            <p>Biggest Threat: ${a.characteristics.biggest_threat}</p>
+            <p>Habitat: ${a.characteristics.habitat}</p>
+            <p>Diet: ${a.characteristics.diet}</p>
+            <p>Lifestyle: ${a.characteristics.lifestyle}</p>
+            <p><em>${a.characteristics.slogan}</em></p>
+            </li>`;
+        })
+            card.innerHTML = templateA;
 
-
+        }
